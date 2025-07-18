8f76285a86c4a70c5ca8b0a1aac96c531d132bf4
Roman Kutselepa <roman.kutselepa@gmail.com>
2025-07-18 16:36:41 +0300

    first commit

---
+++ part1.js
@@ -0,0 +1,66 @@
+// // Ctrl + /
+// // VARIABLES
+// let a;  // undefined
+// a = 15; 
+
+// let b = 10;
+
+// console.log(a);
+// console.log(b);
+
+// a = 5;
+// b = 2;
+// console.log(a);
+// console.log(b);
+
+// const c = 100;
+// console.log(c);
+
+// var d;
+// d = 100;
+// console.log(d);
+
+// var d = 234;
+// console.log(d);
+
+// DATA TYPES
+let stringType = "1234";  // string
+console.log(typeof stringType);
+console.log(Boolean(stringType));  // empty = ""   - is a false value
+
+let numberType = 123145;  // number
+console.log(typeof numberType);
+console.log(Boolean(numberType));  // zero = 0   - is a false value
+
+let booleanType = true;  // boolean (true, false)
+console.log(typeof booleanType);
+
+let nullType = null;  // object (null type)
+console.log(typeof nullType);
+console.log(Boolean(nullType));  // null is always a false value
+
+let undefinedType;
+console.log(typeof undefinedType);  // undefined
+console.log(Boolean(undefinedType));  // undefined is always a false value
+
+let nanType = 10 * 'hello';  // number (NaN - not a number)
+console.log(typeof nanType, nanType);
+console.log(Boolean(nanType));  // NaN is always a false value
+
+
+// let num1 = 10;
+// let num2 = 20;
+
+// console.log(num1 + num2 + num1 + num1 + 123.42 + 23);
+// console.log(num1 + num2 - 5 - 20);
+// console.log(20 - 10 / 5);
+// console.log(10 + 9 % 4);  // 4 + 4 + 1 = 9
+// console.log(9 / 4);
+// console.log(8 % 2);
+
+// console.log((10 * 12) ** 2);
+
+// console.log(num1);
+// num1--;
+// console.log(num1);
+
---
+++ part2.js
@@ -0,0 +1,89 @@
+// strings
+// let someString = 'Hello\nworld';
+//                 //0123456789
+
+// console.log(someString[1]);
+// console.log(someString.length);
+// let x = 12345678;
+// console.log(String(x).length);
+// console.log(x);
+
+// let greet = "Hello world";
+//            //01234
+// console.log(greet.length);
+// console.log(greet[greet.length - 1]);
+
+// let greet = 'that\'s\\';
+// console.log(greet);
+
+// let favouriteBook = "My favourite book is\"Metro 2033\"";
+// console.log(favouriteBook);
+
+// let tick = 'a\tb';
+// console.log(tick);
+
+// console.log('a' === 'а');
+
+// let привет = 'hello';
+// console.log(привет);
+
+// let name = "Jack";
+// let age = 56;
+// let classic = "Hello " + name + ". You are " + age + " years old!";
+// let literal = `Hello ${name.toUpperCase()}. You are ${age} years old!`;
+
+// let html = `
+//             <div>
+
+// </div>
+// `
+
+// console.log(classic);
+// console.log(literal);
+// console.log(html);
+
+// let sample = "      heLLo world world      ";
+
+
+// console.log(sample.toUpperCase());
+// console.log(sample.toLowerCase());
+
+// sample = sample.toLowerCase();
+
+
+// console.log(sample.trim());
+// console.log(sample.trimStart());
+// console.log(sample.trimEnd());
+
+// console.log(sample.replaceAll('world', '').trim()[2]);
+
+// console.log(sample);
+
+// let login = 'terMiNator';
+// let savedLogin = 'Terminator';
+
+// console.log(login.toUpperCase() === savedLogin.toUpperCase());
+
+// let sample2 = "Jack is my name";
+// // let name = sample2[0] + sample2[1] + sample2[2] + sample2[3];
+
+// console.log(sample2.slice(0, 4));
+// console.log(sample2.slice(11, sample2.length));
+// console.log(sample2.slice(-5, -1));
+// console.log(sample2.slice(-1));+
+
+
+// let name = 'jack ';
+// name = name.trim();
+// let normalName = name[0].toUpperCase() + name.slice(1);
+// console.log(normalName);
+
+let greet = "Hello world world";
+
+// console.log(greet.indexOf('world', 11));
+// console.log(greet.lastIndexOf('world'));
+
+
+console.log(greet.includes('world'));
+console.log(greet.startsWith('hello'));
+console.log(greet.endsWith('world'));
