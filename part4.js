a8ce3602548a0048fa8c8a605055ee2dad36c95f
Roman Kutselepa <roman.kutselepa@gmail.com>
2025-08-02 15:49:03 +0300

    if statement

---
+++ part4.js
@@ -0,0 +1,95 @@
+// let x = -10;
+
+// if (x > 0) {
+//     console.log("X is a positive number.");
+// } else if (x === 0) {
+//     console.log("X is a Zero.");
+// } else if (x !== 10) {
+//     console.log("X is not equal to 10.")
+// }
+
+// console.log("Good bye!")
+
+
+let isikukood = "38803160272";
+
+// if (isikukood.length === 11) {
+//     console.log("id", isikukood);
+// } else {
+//     if (isikukood.length < 11) {
+//         console.log("id code is too short");
+//     } else {
+//         console.log("id code is too long");
+//     }
+// }
+
+
+// if (isikukood.length === 11) {
+//     console.log("id", isikukood);
+// } else if (isikukood.length < 11) {
+//     console.log("id code is too short");
+// } else {
+//     console.log("id code is too long");
+// }
+
+// if (isikukood.length === 11) {
+//     console.log("id", isikukood);
+// } else {
+//     console.log("id code must be 11 digits long");
+// }
+
+// let x = 10;
+
+// if (x > 0) {
+//     console.log("x is a positive number");
+// }
+// if (x !== 5) {
+//     console.log("x is not equal to 5");
+// }
+// if (x < 100) {
+//     console.log("x is less than 100");
+// }
+
+// let age = 11;
+
+// if (age >= 0 && age < 12) {  // && - and
+//     console.log("child");
+// }
+// if (age >= 12 && age < 18) {
+//     console.log("teenager");
+// }
+// if (age >= 18 && age < 65) {
+//     console.log("adult");
+// }
+
+// if (age < 12) {
+//     console.log("child");
+// } else if (age < 18) {
+//     console.log("teenager");
+// } else if (age < 65) {
+//     console.log("adult");
+// } else {
+//     console.log("senior");
+// }
+
+// console.log(true || false);
+// console.log(false || true);
+// console.log(false || false);
+// console.log(true || true);
+
+// console.log(true && false || true);
+
+
+// console.log('worlt' < 'world');
+
+let username = 'Terminator';
+
+if (username) {
+    console.log(`Hello ${username}`);
+} else {
+    console.log(`Hello stranger`);
+}
+
+if (username.includes('in')) {
+    console.log('OK')
+}
